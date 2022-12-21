ab6f5c21c9d17303edf9687380839ea4c999205d
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-21 15:59:32 +0100

    Create lib.h

---
+++ lib/my/lib.h
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** xavier.schahl
+** File description:
+** lib.h
+*/
+
+#ifndef LIB_H
+    #define LIB_H
+
+  #include "../../../include/list.h"
+  #include "../../../include/my.h"
+
+#endif /* LIB_H */
