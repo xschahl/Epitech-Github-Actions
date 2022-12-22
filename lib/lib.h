15b2cac4b58a541c8497bee8fac829e4055c4c60
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-22 09:31:21 +0100

    Coding Style lib.h

--- lib/lib.h
+++ lib/lib.h
@@ -5,10 +5,10 @@
 ** lib.h
 */
 
+#include "../include/list.h"
+#include "../include/my.h"
+
 #ifndef LIB_H
     #define LIB_H
-
-  #include "../include/list.h"
-  #include "../include/my.h"
 
 #endif /* LIB_H */
