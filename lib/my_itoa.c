bb3301f0fe78f8b9934f41419b7af7b3d0cde650
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-22 09:48:10 +0100

    Test

--- lib/my_itoa.c
+++ lib/my_itoa.c
@@ -7,31 +7,31 @@
 
 #include "lib.h"
 
-static int my_nbrlen(int nbr)
-{
-    int	div = 1;
-    int count;
+// static int my_nbrlen(int nbr)
+// {
+//     int	div = 1;
+//     int count;
 
-    for (count = 1; nbr / div >= 1; count++)
-        div = div * 10;
-    return (count);
-}
+//     for (count = 1; nbr / div >= 1; count++)
+//         div = div * 10;
+//     return (count);
+// }
 
-char *my_itoa(int nbr)
-{
-    char *string;
-    char *x = "0";
-    int	count = my_nbrlen(nbr) - 1;
+// char *my_itoa(int nbr)
+// {
+//     char *string;
+//     char *x = "0";
+//     int	count = my_nbrlen(nbr) - 1;
 
-    if (nbr == 0)
-        return x;
-    if ((string = malloc(sizeof(char) * my_nbrlen(nbr))) == NULL)
-        return NULL;
-    count = my_nbrlen(nbr) - 1;
-    string[count] = '\0';
-    for (int div = 10; count > 0; count--) {
-        string[count - 1] = (nbr % div) + 48;
-        nbr = nbr / 10;
-    }
-    return string;
-}
+//     if (nbr == 0)
+//         return x;
+//     if ((string = malloc(sizeof(char) * my_nbrlen(nbr))) == NULL)
+//         return NULL;
+//     count = my_nbrlen(nbr) - 1;
+//     string[count] = '\0';
+//     for (int div = 10; count > 0; count--) {
+//         string[count - 1] = (nbr % div) + 48;
+//         nbr = nbr / 10;
+//     }
+//     return string;
+// }
