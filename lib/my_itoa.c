7dfffca949600f8f57427d4d600c0bc388441bde
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-22 13:29:16 +0100

    Test

--- lib/my_itoa.c
+++ lib/my_itoa.c
@@ -21,13 +21,12 @@ char *my_itoa(int nbr)
 {
     char *string;
     char *x = "0";
-    int	count;
+    int	count = my_nbrlen(nbr) - 1;
 
     if (nbr == 0)
         return x;
     if ((string = malloc(sizeof(char) * my_nbrlen(nbr))) == NULL)
         return NULL;
-    count = my_nbrlen(nbr) - 1;
     string[count] = '\0';
     for (int div = 10; count > 0; count--) {
         string[count - 1] = (nbr % div) + 48;
