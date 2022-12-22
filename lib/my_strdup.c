38b92ace155010a25bc56b071a5448dec1f5689d
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-22 09:44:18 +0100

    Test

--- lib/my_strdup.c
+++ lib/my_strdup.c
@@ -12,9 +12,8 @@ char *my_strdup(char const *src)
     int x = 0;
     char *result;
 
-    while (src[x] != '\0')  {
+    while (src[x] != '\0')
         x++;
-    }
     result = malloc(sizeof(char) * (x + 1));
     result = my_strcpy(result, src);
     return (result);
