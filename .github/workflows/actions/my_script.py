f55f330e9fd031d1b2d12bc14018fc927a8981a6
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-23 13:49:24 +0100

    Update my_script.py

--- .github/workflows/actions/my_script.py
+++ .github/workflows/actions/my_script.py
@@ -1,3 +1,8 @@
 import sys;
 
-sys.stdout.write("mirror_url=fezjfazifjzfjafazfjfz");
+URL = sys.argv[2]
+if sys.argv[1] == '':
+    URL = URL.split("/")[1]
+    sys.stdout.write("mirror_url=" + URL)
+else:
+    sys.stdout.write("mirror_url=" + sys.argv[1])
