b6c7817eb16c0fc909ce82624be392b89c85c675
xavier_schahl <91027790+xschahl@users.noreply.github.com>
2022-12-23 14:02:07 +0100

    Update my_script.py

--- .github/workflows/actions/my_script.py
+++ .github/workflows/actions/my_script.py
@@ -1,4 +1,4 @@
 import sys;
 
-URL = "git@github.com:EpitechPromo2026/" + URL.split("/")[1] + ".git"
+URL = "git@github.com:EpitechPromo2026/" + sys.argv[1].split("/")[1] + ".git"
 sys.stdout.write("mirror_url=" + URL)
